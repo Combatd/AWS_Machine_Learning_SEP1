1865bbde63c5bbda025f34f782330a1b24a24516
Combatd <combatdstar@gmail.com>
2020-05-23 07:24:27 -0700

    Create function rename_columns(data_file) for modularity

--- refactoring_wine_quality/wine_analysis.py
+++ refactoring_wine_quality/wine_analysis.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # In [1]
-df = pd.read_csv('winequality-red.csv', sep=';')
-df.head()
+# df = pd.read_csv('winequality-red.csv', sep=';')
+# df.head()
 
 # Out[1]
 # 	fixed acidity	volatile acidity	citric acid	residual sugar	chlorides	free sulfur dioxide	total sulfur dioxide	density	pH	sulphates	alcohol	quality
@@ -11,3 +11,10 @@ df.head()
 # 2	7.8	0.76	0.04	2.3	0.092	15.0	54.0	0.9970	3.26	0.65	9.8	5
 # 3	11.2	0.28	0.56	1.9	0.075	17.0	60.0	0.9980	3.16	0.58	9.8	6
 # 4	7.4	0.70	0.00	1.9	0.076	11.0	34.0	0.9978	3.51	0.56	9.4
+
+# refactored for modularity
+def rename_columns(data_file):
+    df = pd.read_csv(data_file, sep=';')
+    return df.head()
+
+rename_columns('winequality-red.csv')
