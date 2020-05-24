e71bd36eac71cde1a3524c74303bcdf685fb62e7
Combatd <combatdstar@gmail.com>
2020-05-24 13:30:49 -0700

    Add optimizing_holiday_gifts quiz

---
+++ optimizing_holiday_gifts/gift_costs.py
@@ -0,0 +1,20 @@
+import time
+import numpy as np
+
+with open('gift_costs.txt') as f:
+    gift_costs = f.read().split('\n')
+    
+gift_costs = np.array(gift_costs).astype(int)  # convert string to int
+
+start = time.time()
+
+total_price = 0
+for cost in gift_costs:
+    if cost < 25:
+        total_price += cost * 1.08  # add cost after tax
+
+print(total_price)
+print('Duration: {} seconds'.format(time.time() - start))
+
+# 32765421.24
+# Duration: 5.311075210571289 seconds
